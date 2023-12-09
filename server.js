5c401bd3a0bb53b4899b689142245fe06a9b87e5
Tin9oo <rlawnghks99@gmail.com>
2023-12-09 11:53:09 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,25 @@ const port = process.env.PORT || 5001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          'id': 1,
-          'image': 'https://picsum.photos/id/1/64/64',
-          'name': '김주환',
-          'birthday': '990601',
-          'gender': 'Male',
-          'job': 'Under graduate'
-        },
-        {
-          'id': 2,
-          'image': 'https://picsum.photos/id/2/64/64',
-          'name': '홍길동',
-          'birthday': '000000',
-          'gender': 'Male',
-          'job': '의적'
-        },
-        {
-          'id': 3,
-          'image': 'https://picsum.photos/id/3/64/64',
-          'name': '이순신',
-          'birthday': '000001',
-          'gender': 'Male',
-          'job': '해군'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-      ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
